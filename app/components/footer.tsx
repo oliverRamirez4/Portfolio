9088f6ee4b1986f93bf2b6c39ae18d10a60d9fc5
oliverRamirez4 <oliverramirez307@gmail.com>
2025-01-09 22:54:54 -0700

    routing github and linkedin links to new tab

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,10 +7,10 @@ export function Footer() {
     return (
         <nav>
             <div className='flex mt-16'>
-                <Link href='https://www.linkedin.com/in/oliver-ramirez04/' className='px-6'>
+                <Link href='https://www.linkedin.com/in/oliver-ramirez04/' target="_blank" className='px-6'>
                     <FaLinkedin className = 'scale-150'/> 
                 </Link>
-                <Link href='https://github.com/oliverRamirez4' className='px-6 flex'>
+                <Link href='https://github.com/oliverRamirez4' target="_blank"  className='px-6 flex'>
                     <FaGithub className = 'scale-150' /> 
                 </Link>
 

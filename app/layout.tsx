a75b3f1a5e6fc54cd915a41b4b124a5893bbf5dd
oliverRamirez4 <oliverramirez307@gmail.com>
2025-01-13 22:47:03 -0700

    remove export of geistMono

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Footer } from "./components/footer";
 
 
 
-export const geistMono = Geist_Mono({
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -29,7 +29,7 @@ export default function RootLayout({
           <NavBar />
           {children}
           <Footer />
-          
+
         </main>
 
       </body>

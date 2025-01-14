e7b37780d63d6b1305b6390f31a204031e06f9a3
oliverRamirez4 <oliverramirez307@gmail.com>
2025-01-13 22:37:20 -0700

    fix eslint error

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <section>
-      <h1 className='text-2xl font-bold mb-6'>
+      <h1 className="text-2xl font-bold mb-6">
         My Portfolio
       </h1>
       <p>
